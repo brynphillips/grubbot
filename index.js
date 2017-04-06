5005d34ced68f121bc4501e473440a28219c7e66
Bryn <brynphillips1@gmail.com>
2017-04-06 12:58:31 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -122,9 +122,12 @@ app.post('/messages', function (req, res) {
 });
 app.listen(8080);
 
-setTimeout(function() {slack.chat.postMessage()}, 1000);
+function postMessage() {
+    slack.postMessage(slack.chat.postMessage);
+}
 
-
+// post a message every 5 minutes (5 mins * 60 seconds * 1000 milliseconds)
+setInterval(postMessage, 5 * 60 * 1000);
 
 
 
